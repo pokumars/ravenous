f549024b6e781cfd522588a8ae4e63c8066c1eb4
Oheneba <ohenebap@metropolia.fi>
2019-06-20 06:52:46 +0300

    business yelp page and google maps links added

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import './Business.css'
-import ReactDOM from 'react-dom';
 
 
 class Business extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.googleMapsPage =`http://maps.google.com/?q=${this.props.business.address}, ${this.props.business.city}, ${this.props.business.state}, ${this.props.business.zipCode}`;
+    }
+    
+
+
     render(){
         return (
 
             <div className="Business">
                 <div className="image-container">
-                    <img src={this.props.business.imageSrc} alt=''/>
+                    <a href={this.props.business.url} target='_blank'><img src={this.props.business.imageSrc} alt=''/></a>
                 </div>
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
-                    <div className="Business-address">
-                    <p>{this.props.business.address}</p>
-                    <p>{this.props.business.city}</p>
-                    <p>{this.props.business.state} {this.props.business.zipCode}</p>
-                    </div>
+                    <a href={this.googleMapsPage} target="_blank">
+                        <div className="Business-address">
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        </div>
+                    </a>
                     <div className="Business-reviews">
                     <h3>{this.props.business.category}</h3>
                     <h3 className="rating">{this.props.business.rating} stars</h3>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,8 +13,10 @@ class SearchBar extends React.Component {
 
       this.handleTermChange = this.handleTermChange.bind(this);
       this.handleLocationChange = this.handleLocationChange.bind(this);
+      this.handleSortByChange = this.handleSortByChange.bind(this);
       this.handleSearch = this.handleSearch.bind(this);
       this.pressedEnter = this.pressedEnter.bind(this);
+      
 
       this.sortByOptions = {
         'Best Match': 'best_match',
@@ -86,8 +88,8 @@ class SearchBar extends React.Component {
             </ul>
           </div>
           <div className="SearchBar-fields">
-            <input placeholder="Search Businesses" onChange={this.handleTermChange} onKeyPress={this.pressedEnter} />
-            <input placeholder="Where?" onChange={this.handleLocationChange} onKeyPress={this.pressedEnter} />
+            <input placeholder="Search Businesses e.g. Sushi" onChange={this.handleTermChange} onKeyPress={this.pressedEnter} />
+            <input placeholder="Where? e.g Seattle" onChange={this.handleLocationChange} onKeyPress={this.pressedEnter} />
           </div>
           <div className="SearchBar-submit">
             <a type="submit" onClick={this.handleSearch}>Let's Go</a>
