ffb6fbf8632e432c3af6aa954ae608ed97b0ad10
pokumars <ohenebakob@hotmail.com>
2019-06-22 13:09:01 +0300

    Updates

---
+++ precache-manifest.380907b95abdf9fc553be8bc8f8d7f91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442bf16761ab95ae62d0468bc560a4e3",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "50b19779c03194e23ef2",
+    "url": "/ravenous/static/css/main.dd88ca46.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/ravenous/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "50b19779c03194e23ef2",
+    "url": "/ravenous/static/js/main.55f30e2a.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ravenous/static/media/logo.ee7cd8ed.svg"
+  }
+]);
